4c2f1453cf50c1712878f92f11c16566031f3b19
Tyler <ArKaes1820@yahoo.com>
2022-11-18 09:29:29 -0800

    Added animation to the skills section

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ var currentYear = new Date().getFullYear();
 
 copyRightElement.innerHTML = `&copy; ${currentYear} Tyler Montgomery <a href='https://github.com/ArKaes13' style='text-decoration:none; color:white;'><i class="fa-brands fa-github"></a></i>`
 
-// Animates the resume section
-function debounce(func, wait = 10, immediate = true) {
+// Debounce function for limiting the amount of scroll events that are triggered
+function debounce(func, wait = 15, immediate = true) {
     var timeout;
     return function() {
         var context = this, args = arguments;
@@ -20,9 +20,29 @@ function debounce(func, wait = 10, immediate = true) {
     };
 };
 
+// Handles animation on scroll for skills and resume sections
+const skillImages = document.querySelectorAll('.skill-image');
 const jobDivs = document.querySelectorAll('.slide-in');
 
-function checkSlide(event) {
+function checkSlide() {
+    for (i = 0; i < skillImages.length; i++) {
+        var slideInAt = (window.scrollY + window.innerHeight) - 
+            skillImages[i].height / 2;
+        var imageBottom = skillImages[i].offsetTop + skillImages[i].height;
+        var isHalfShown = slideInAt > skillImages[i].offsetTop;
+        var isNotScrolledPast = window.scrollY < imageBottom;
+
+        if (isHalfShown && isNotScrolledPast) {
+            skillImages[i].style.animationDelay = `${.3 * i}s`;
+            skillImages[i].classList.remove('inactive');
+            skillImages[i].classList.add('active');
+        } else {
+            skillImages[i].classList.remove('active');
+            skillImages[i].style.animationDelay = `0s`;
+            skillImages[i].classList.add('inactive');
+        }
+    }
+    
     for (div of jobDivs) {
         var slideInAt = (window.scrollY + window.innerHeight) - 
             div.clientHeight / 2;
